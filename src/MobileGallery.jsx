0e0a63353b7ef5393f15604d7a8ef8d0a3af5c7a
Rhema <reymarmcoc@gmail.com>
2025-11-03 12:49:04 +0100

    neww

--- src/MobileGallery.jsx
+++ src/MobileGallery.jsx
@@ -1,8 +1,8 @@
 import DomeGallery from "./components/DomeGallery.jsx";
 export default function MobileGallery() {
   return (
-    <div className=" overflow-x-hidden max-md:w-80 max-md:h-80 w-[80vw] h-[80vh]">
-      <DomeGallery fit={0.5} minRadius={300} segments={20} />
+    <div className=" overflow-x-hidden max-md:w-screen max-md:h-[50vh] w-[80vw] h-[80vh]">
+      <DomeGallery fit={0.5} minRadius={250} segments={15} />
     </div>
   );
 }
